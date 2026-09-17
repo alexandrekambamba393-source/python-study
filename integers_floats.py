2c6995a0ef871ae1457c2571b573ae97221c3e25
alexandrekambamba393-source <alexandrekambamba393@gmail.com>
2026-09-17 16:25:58 +0300

    your message

---
+++ integers_floats.py
@@ -0,0 +1,57 @@
+# Integers ->(class int)
+num1=1000
+num2=20
+print(type(num1))
+
+# Addition 
+total=num1+num2
+print(total)
+
+# Subtraction
+min=num1-num2
+print(min)
+
+# Multiplication
+mult=num1*num2
+print(mult)
+
+# Division
+div=num1/num2
+print(div)
+
+# Modulus(%) returns the remainder of a division operation
+num1=1000
+num2=20
+mod=num1%num2
+print(mod)
+
+num1=1015
+num2=20
+mod=num1%num2
+print(mod)
+
+# Exponentiation(*) 
+num1=10
+num2=3
+exp=num1**num2
+print(exp)
+
+y=10**2
+num3=num1**num2
+print(num3)
+
+# Floor Operation(//) returns the divisible number of a division ignoring the remainder 
+num1=1015
+num2=20
+floor=num1//num2
+print(floor)
+
+            # floats
+num4=995.623
+# Round()
+num5=round(num4)
+print(num5)
+
+#For specify the number of decimal places to round to, you can use the second argument of the round() function. For example, to round num4 to 2 decimal places:
+rounded_num4 = round(num4, 2)
+print(rounded_num4)
--- stringtask.py
+++ stringtask.py
@@ -1,7 +1,8 @@
 # Clean up the following variable to give the clean version in lower case. Using inbuilt methods in the str class 
-name ="  JOHn  " "to" "john"
+name ="  JOHn." "to" "john"
 
-classname="  JOHn" 
+classname="  JOHn." 
+classname=classname.replace(".","")
 classname=classname.strip()
 classname=classname.lower()
 print(classname)
@@ -17,10 +18,24 @@ print(sentence_two[16:30])
 
 sentence_three="The lazy dog; ran so fast; it hit the wall."
 sentence_three=sentence_three.split(";")
+print(sentence_three)
 print(len(sentence_three))
 
 #first_name="  Joh.n"  last_name="   Do,e" Clean up and display Full name i.e John Doe
-first_name="  Joh.n"
-last_name="   Do,e"
-print(first_name.strip().replace(".","")+" "+last_name.strip().replace(",",""))
 
+first_name="  Joh.n"
+first_name=first_name.strip().replace(".","")
+
+last_name="   Do,e"
+last_name=last_name.strip().replace(",","")
+
+full_name=first_name+" "+last_name
+print(full_name)
+
+#Having the string r = '["E","W","C"]' #Manipulate it to display EWC
+r = '["E","W","C"]'
+r = r.replace(",","")
+r = r.replace("[","")
+r = r.replace("]","")
+r = r.replace('"',"")
+print(r)
